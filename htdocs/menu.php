f03764d0e5f3cc327296f4441518d5a2c1ead313
Clifford Kent S. Aganap <aganap.cliffordkent@ici.edu.ph>
2024-11-15 14:44:34 +0000

    Kupal

---
+++ htdocs/menu.php
@@ -0,0 +1,14 @@
+
+<nav>
+    <ul>
+        <li><a href="home.php">Home</a></li>
+        <li><a href="Teams.php">Teams</a></li>
+        <li><a href="Player_list.php">View Players</a></li>
+        <li><a href="Games.php">Games</a></li>
+    </ul>
+</nav>
+
+
+
+
+
